b6c0748bfe047c582d373a9af9676f8cfca95fcc
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-15 00:37:33 +0000

    Created partials, views with templates and printed shorten url with actual url

--- express_server.js
+++ express_server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.set("view engine", "ejs")
 
-var urlDatabase ={
+var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+    urls:urlDatabase
+   };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.end("Hello!\n");
