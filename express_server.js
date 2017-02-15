50bbb8b0968177dcd245ba3869d522fa4697f2eb
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-15 02:28:15 +0000

    Redirected after form submission

--- express_server.js
+++ express_server.js
@@ -2,11 +2,45 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.set("view engine", "ejs")
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+function generateRandomString() {
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var total = ""
+  for ( var i = 0; i < 6; i++) {
+    min = Math.ceil(1);
+    max = Math.floor(chars.length-1);
+    random = Math.floor(Math.random() *(chars.length-1))+1;
+    total += chars[random];
+  }
+  return total;
+}
+
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let temp = generateRandomString()
+  urlDatabase[temp] = req.body.longURL
+  console.log(req.body);
+  res.redirect("http://localhost:8080/urls/" + temp);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(longURL);
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
